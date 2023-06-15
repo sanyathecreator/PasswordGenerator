a15b6f5841a3946f6196cad127c76e4f31a9f618
sanyathecreator <olexxxandr.romaniuk@gmail.conm>
2023-06-15 20:53:46 +0200

    add main functionality

--- PasswordGenerator/Form1.Designer.cs
+++ PasswordGenerator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.passwordLengthLabel = new System.Windows.Forms.Label();
             this.symbolsLabel = new System.Windows.Forms.Label();
             this.numbersLabel = new System.Windows.Forms.Label();
@@ -47,9 +48,10 @@
             // 
             this.passwordLengthLabel.AutoSize = true;
             this.passwordLengthLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.passwordLengthLabel.Location = new System.Drawing.Point(60, 30);
+            this.passwordLengthLabel.Location = new System.Drawing.Point(80, 37);
+            this.passwordLengthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.passwordLengthLabel.Name = "passwordLengthLabel";
-            this.passwordLengthLabel.Size = new System.Drawing.Size(149, 21);
+            this.passwordLengthLabel.Size = new System.Drawing.Size(185, 27);
             this.passwordLengthLabel.TabIndex = 0;
             this.passwordLengthLabel.Text = " Password Length:";
             // 
@@ -57,9 +59,10 @@
             // 
             this.symbolsLabel.AutoSize = true;
             this.symbolsLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.symbolsLabel.Location = new System.Drawing.Point(71, 70);
+            this.symbolsLabel.Location = new System.Drawing.Point(95, 86);
+            this.symbolsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.symbolsLabel.Name = "symbolsLabel";
-            this.symbolsLabel.Size = new System.Drawing.Size(138, 21);
+            this.symbolsLabel.Size = new System.Drawing.Size(174, 27);
             this.symbolsLabel.TabIndex = 1;
             this.symbolsLabel.Text = "Include Symbols:";
             // 
@@ -67,9 +70,10 @@
             // 
             this.numbersLabel.AutoSize = true;
             this.numbersLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numbersLabel.Location = new System.Drawing.Point(64, 110);
+            this.numbersLabel.Location = new System.Drawing.Point(85, 135);
+            this.numbersLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.numbersLabel.Name = "numbersLabel";
-            this.numbersLabel.Size = new System.Drawing.Size(145, 21);
+            this.numbersLabel.Size = new System.Drawing.Size(180, 27);
             this.numbersLabel.TabIndex = 2;
             this.numbersLabel.Text = "Include Numbers:";
             this.numbersLabel.Click += new System.EventHandler(this.numbersLabel_Click);
@@ -78,107 +82,123 @@
             // 
             this.uppercaseLabel.AutoSize = true;
             this.uppercaseLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.uppercaseLabel.Location = new System.Drawing.Point(54, 150);
+            this.uppercaseLabel.Location = new System.Drawing.Point(72, 185);
+            this.uppercaseLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.uppercaseLabel.Name = "uppercaseLabel";
-            this.uppercaseLabel.Size = new System.Drawing.Size(155, 21);
+            this.uppercaseLabel.Size = new System.Drawing.Size(193, 27);
             this.uppercaseLabel.TabIndex = 3;
             this.uppercaseLabel.Text = "Include Uppercase:";
             // 
             // generateButton
             // 
             this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.generateButton.Location = new System.Drawing.Point(250, 225);
+            this.generateButton.Location = new System.Drawing.Point(333, 277);
+            this.generateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.generateButton.Name = "generateButton";
-            this.generateButton.Size = new System.Drawing.Size(100, 50);
+            this.generateButton.Size = new System.Drawing.Size(133, 62);
             this.generateButton.TabIndex = 4;
             this.generateButton.Text = "Generate";
             this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
             // 
             // resultLabel
             // 
             this.resultLabel.AutoSize = true;
             this.resultLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.resultLabel.Location = new System.Drawing.Point(100, 290);
+            this.resultLabel.Location = new System.Drawing.Point(133, 357);
+            this.resultLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(60, 21);
+            this.resultLabel.Size = new System.Drawing.Size(74, 27);
             this.resultLabel.TabIndex = 5;
             this.resultLabel.Text = "Result:";
             // 
             // symbolsCB
             // 
             this.symbolsCB.AutoSize = true;
-            this.symbolsCB.Location = new System.Drawing.Point(215, 74);
+            this.symbolsCB.Location = new System.Drawing.Point(287, 91);
+            this.symbolsCB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.symbolsCB.Name = "symbolsCB";
-            this.symbolsCB.Size = new System.Drawing.Size(88, 17);
+            this.symbolsCB.Size = new System.Drawing.Size(105, 20);
             this.symbolsCB.TabIndex = 6;
             this.symbolsCB.Text = " (e.g. @#$%)";
             this.symbolsCB.UseVisualStyleBackColor = true;
+            this.symbolsCB.CheckedChanged += new System.EventHandler(this.symbolsCB_CheckedChanged);
             // 
             // numbersCB
             // 
             this.numbersCB.AutoSize = true;
-            this.numbersCB.Location = new System.Drawing.Point(215, 114);
+            this.numbersCB.Location = new System.Drawing.Point(287, 140);
+            this.numbersCB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numbersCB.Name = "numbersCB";
-            this.numbersCB.Size = new System.Drawing.Size(86, 17);
+            this.numbersCB.Size = new System.Drawing.Size(100, 20);
             this.numbersCB.TabIndex = 7;
             this.numbersCB.Text = " (e.g. 12345)";
             this.numbersCB.UseVisualStyleBackColor = true;
+            this.numbersCB.CheckedChanged += new System.EventHandler(this.numbersCB_CheckedChanged);
             // 
             // uppercaseCB
             // 
             this.uppercaseCB.AutoSize = true;
-            this.uppercaseCB.Location = new System.Drawing.Point(215, 154);
+            this.uppercaseCB.Location = new System.Drawing.Point(287, 190);
+            this.uppercaseCB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.uppercaseCB.Name = "uppercaseCB";
-            this.uppercaseCB.Size = new System.Drawing.Size(98, 17);
+            this.uppercaseCB.Size = new System.Drawing.Size(119, 20);
             this.uppercaseCB.TabIndex = 8;
             this.uppercaseCB.Text = " (e.g. ABCDEF)";
             this.uppercaseCB.UseVisualStyleBackColor = true;
+            this.uppercaseCB.CheckedChanged += new System.EventHandler(this.uppercaseCB_CheckedChanged);
             // 
             // hScrollBar
             // 
             this.hScrollBar.LargeChange = 1;
-            this.hScrollBar.Location = new System.Drawing.Point(212, 31);
+            this.hScrollBar.Location = new System.Drawing.Point(283, 38);
             this.hScrollBar.Maximum = 16;
             this.hScrollBar.Minimum = 4;
             this.hScrollBar.Name = "hScrollBar";
-            this.hScrollBar.Size = new System.Drawing.Size(160, 20);
+            this.hScrollBar.Size = new System.Drawing.Size(213, 20);
             this.hScrollBar.TabIndex = 1;
             this.hScrollBar.Value = 4;
+            this.hScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar_Scroll);
             // 
             // textBox
             // 
             this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox.Location = new System.Drawing.Point(167, 290);
+            this.textBox.Location = new System.Drawing.Point(223, 357);
+            this.textBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox.Name = "textBox";
             this.textBox.ReadOnly = true;
-            this.textBox.Size = new System.Drawing.Size(269, 22);
+            this.textBox.Size = new System.Drawing.Size(357, 26);
             this.textBox.TabIndex = 9;
             // 
             // copyButton
             // 
             this.copyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.copyButton.Location = new System.Drawing.Point(442, 289);
+            this.copyButton.Location = new System.Drawing.Point(589, 356);
+            this.copyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.copyButton.Name = "copyButton";
-            this.copyButton.Size = new System.Drawing.Size(60, 25);
+            this.copyButton.Size = new System.Drawing.Size(80, 31);
             this.copyButton.TabIndex = 10;
             this.copyButton.Text = "Copy";
             this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
             // 
             // scrollBarValue
             // 
             this.scrollBarValue.AutoSize = true;
             this.scrollBarValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.scrollBarValue.Location = new System.Drawing.Point(375, 34);
+            this.scrollBarValue.Location = new System.Drawing.Point(500, 42);
+            this.scrollBarValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scrollBarValue.Name = "scrollBarValue";
-            this.scrollBarValue.Size = new System.Drawing.Size(36, 16);
+            this.scrollBarValue.Size = new System.Drawing.Size(61, 20);
             this.scrollBarValue.TabIndex = 11;
-            this.scrollBarValue.Text = "Size:";
+            this.scrollBarValue.Text = "Size: 4";
+            this.scrollBarValue.Click += new System.EventHandler(this.scrollBarValue_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(779, 444);
             this.Controls.Add(this.scrollBarValue);
             this.Controls.Add(this.copyButton);
             this.Controls.Add(this.textBox);
@@ -192,9 +212,12 @@
             this.Controls.Add(this.numbersLabel);
             this.Controls.Add(this.symbolsLabel);
             this.Controls.Add(this.passwordLengthLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Password Generator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- PasswordGenerator/Form1.cs
+++ PasswordGenerator/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,14 @@ namespace PasswordGenerator
 {
     public partial class Form1 : Form
     {
+        bool includeSymbols = false;
+        bool includeNumbers = false;
+        bool includeUppercase = false;
+
+        string symbols = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}";
+        string numbers = "1234567890";
+        string uppercase = "qwertyuiopasdfghjklzxcvbnm".ToUpper();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +28,73 @@ namespace PasswordGenerator
 
         private void numbersLabel_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void hScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            int scrollPosition = hScrollBar.Value;
+            Console.WriteLine(scrollPosition);
+            scrollBarValue.Text = $"Size: {scrollPosition}";
+        }
+
+        private void scrollBarValue_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void symbolsCB_CheckedChanged(object sender, EventArgs e)
+        {
+            includeSymbols = symbolsCB.Checked;
+        }
+
+        private void numbersCB_CheckedChanged(object sender, EventArgs e)
+        {
+            includeNumbers = numbersCB.Checked;
+        }
+
+        private void uppercaseCB_CheckedChanged(object sender, EventArgs e)
+        {
+            includeUppercase = uppercaseCB.Checked;
+        }
+
+        private string GeneratePassword()
+        {
+            Random random = new Random();
+            string letters = "qwertyuiopasdfghjklzxcvbnm";
+            if (includeUppercase) 
+            {
+                letters += uppercase;
+            }
+            if (includeNumbers)
+            {
+                letters += numbers;
+            }
+            if(includeSymbols)
+            {
+                letters += symbols;
+            }
+            char[] chars = letters.ToCharArray();
+            letters = "qwertyuiopasdfghjklzxcvbnm";
+            string password = "";
+            for(int i = 0; i < hScrollBar.Value; i++)
+            {
+                password += chars[random.Next(0, chars.Length)];
+            }
+            return password;
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            textBox.Text = GeneratePassword();
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            if(textBox.Text.Length > 0)
+            {
+                Clipboard.SetText(textBox.Text);
+            }
         }
     }
 }
